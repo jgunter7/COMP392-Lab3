dde4ddee0ff2cbe1c81a75ea7df0d412d81d87d1
jgunter7 <jason_gunter@hotmail.com>
2016-02-08 18:02:03 -0500

    refresh

--- README.md
+++
@@ -1,3 +0,0 @@
-# typescriptgulp
-
-Example project to transpile TypeScript to JavaScript by using Gulp Automation
--- Scripts/_reference.js
+++
@@ -1,4 +0,0 @@
-/// <reference path="../typings/tsd.d.ts"/>
-/// <reference path="objects/person.ts"/> 
-
-//# sourceMappingURL=_reference.js.map
